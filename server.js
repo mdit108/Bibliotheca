29e55443114af9cccc0131d23fde5a1a5f49d749
Mudit Nawalgaria <mudit.nawalgaria@gmail.com>
2021-09-10 14:24:34 +0530

    Books Create/Index/New

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const expressLayouts = require ("express-ejs-layouts");
 
 const indexRouter = require ("./routes/index")
 const authorRouter = require ("./routes/authors")
+const bookRouter = require ("./routes/books")
 
 
 app.set("view engine","ejs");
@@ -28,5 +29,6 @@ db.once('open',()=> console.log("Connected to mongoose"));
 
 app.use('/',indexRouter);
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3000)
