2ce8747af6aeb0af11943038aabe0387bd015cbe
Mudit Nawalgaria <mudit.nawalgaria@gmail.com>
2021-09-12 12:11:48 +0530

    Finished author routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV!=='production'){
 const express = require("express");
 const app = express();
 const expressLayouts = require ("express-ejs-layouts");
+const methodOverride = require ('method-override');
 
 
 const indexRouter = require ("./routes/index")
@@ -19,6 +20,7 @@ app.use (expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({ limit:'10mb',extended:false }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 const mongoose = require ("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {  useNewUrlParser: true,
